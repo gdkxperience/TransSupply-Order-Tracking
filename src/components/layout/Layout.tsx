7dbe0d548ca2a0e8b38d481f25719c436327c350
Georgi Krastev <krustevgeorgi@yahoo.com>
2026-01-28 18:56:15 +0200

    Revert "Add container width for better medium responsiveness"
    This reverts commit fe25cb182e60b7db2fa0a924df4ad5b19c7f4b82.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -26,7 +26,6 @@ export function Layout({ children }: LayoutProps) {
         
         <div className="p-4 md:p-6">
           <motion.div
-            className="w-full max-w-[1400px] mx-auto"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.1 }}
